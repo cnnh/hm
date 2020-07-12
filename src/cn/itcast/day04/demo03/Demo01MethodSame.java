073bbd8116ebd8ce330762d4c6b2e5d7af394354
false07 <984110211@qq.com>
2020-07-12 14:11:03 +0800

    定义一个方法，用来判断两个数字是否相同

--- src/cn/itcast/day04/demo03/Demo01MethodSame.java
+++ src/cn/itcast/day04/demo03/Demo01MethodSame.java
@@ -1,4 +1,20 @@
 package cn.itcast.day04.demo03;
-
+/*
+定义一个方法，用来判断两个数字是否相同
+ */
 public class Demo01MethodSame {
+    public static void main(String[] args) {
+        System.out.println("isSame结果"+isSame(34,55));
+    }
+    public  static  boolean isSame(int a, int b){
+//        boolean same;
+//        if(a==b){
+//            same=true;
+//        }else{
+//            same=false;
+//        }
+        boolean same = a==b?true:false;
+//        boolean same =a==b;
+        return  same;
+    }
 }
